b78b798f1f46ca4318140795bf1a8fc0c400cd10
lmq <2965892623@qq.com>
2021-10-20 19:36:10 +0800

    STRUCTURE-SORT-1.5.3

--- src/main/java/com/lmq/sort/ShellSort.java
+++ src/main/java/com/lmq/sort/ShellSort.java
@@ -4,7 +4,10 @@ import java.util.Date;
 import java.util.Random;
 
 /**
- * 希尔排序(交换)
+ * 1，希尔排序(交换)
+ * 根据数组长度确定步长，根据步长分组比较
+ *
+ * 2，希尔排序(插入)
  * 根据数组长度确定步长，根据步长分组比较
  */
 public class ShellSort {
@@ -15,9 +18,15 @@ public class ShellSort {
             array[i] = random.nextInt();
         }
         Date begin = new Date(System.currentTimeMillis());
-        sortByChange(array);
+        //sortByChange(array);//6,8,8
+        sortByInsert(array);//0.015,0.014,0.013
         Date end = new Date(System.currentTimeMillis());
         System.out.println((end.getTime() - begin.getTime()));
+//        int[] array = new int[]{5,2,3,9,7,1,6,8,4};
+//        sortByInsert(array);
+//        for (int i : array) {
+//            System.out.print(i + " ");
+//        }
     }
 
     public static void sortByChange(int[] array) {
@@ -30,6 +39,22 @@ public class ShellSort {
                         array[j] = array[j + gap];
                         array[j + gap] = temp;
                     }
+                }
+            }
+        }
+    }
+
+    public static void sortByInsert(int[] array){
+        for (int gap = array.length / 2; gap > 0; gap /= 2) {//分几次组，最后一次步长是1
+            for (int i = gap; i < array.length; i++) {//对所有组循环，以所有组的第二个元素开始为索引
+                int insertIndex = i;
+                int insertVal = array[insertIndex];
+                if(array[insertIndex] < array[insertIndex - gap]){
+                    while (insertIndex - gap >= 0 && insertVal < array[insertIndex - gap]){
+                        array[insertIndex] = array[insertIndex - gap];
+                        insertIndex -= gap;
+                    }
+                    array[insertIndex] = insertVal;
                 }
             }
         }
