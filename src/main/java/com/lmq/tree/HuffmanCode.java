61ae561443f6f5b54931d6fdf9ccadf08e481b77
lmq <2965892623@qq.com>
2021-10-27 13:25:02 +0800

    STRUCTURE-TREE-1.7.10

--- src/main/java/com/lmq/tree/HuffmanCode.java
+++ src/main/java/com/lmq/tree/HuffmanCode.java
@@ -1,16 +1,21 @@
 package com.lmq.tree;
 
+import javax.crypto.CipherInputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class HuffmanCode {
     private static StringBuilder code = new StringBuilder();
     private static Map<Byte,String> huffmanCodes = new HashMap<>();
+    private static int zeroCount = 0;//防止最后一个字节丢失'0'！！！！！！
     public static void main(String[] args) {
-        String str = "i like like like java do you like a java";
-        byte[] strBytes = str.getBytes(StandardCharsets.UTF_8);
-        byte[] result = huffmanZip(strBytes);
-        System.out.println(Arrays.toString(result));
+//        String input = "D:\\IDEA\\my-algorithm-test\\src\\main\\resources\\static\\CircleArray.png";
+//        String output = "D:\\IDEA\\my-algorithm-test\\src\\main\\resources\\zip\\CircleArray.zip";
+//        zipFile(input,output);
+        String input = "D:\\IDEA\\my-algorithm-test\\src\\main\\resources\\zip\\CircleArray.zip";
+        String output = "D:\\IDEA\\my-algorithm-test\\src\\main\\resources\\unzip\\CircleArray.png";
+        unZip(input,output);
     }
     /**
      * 将一个字符串所有字符统计
@@ -97,6 +102,7 @@ public class HuffmanCode {
      */
     public static byte[] zip(byte[] bytes,Map<Byte,String> huffmanCodes){
         int index = 0;
+        //生成二进制字符串
         StringBuilder builder = new StringBuilder();
         for (byte b : bytes) {
             builder.append(huffmanCodes.get(b));
@@ -106,6 +112,24 @@ public class HuffmanCode {
             String sub = "";
             if(i+8 > builder.length()){
                 sub = builder.substring(i);
+                for (int k = 0;k < sub.length();k++){
+                    if(sub.charAt(k) == '1'){
+                        break;
+                    }
+                    if(sub.charAt(k) == '0'){
+                        zeroCount++;
+                    }
+                }
+            }else if (i+8 == builder.length()){
+                sub = builder.substring(i);
+                for (int k = 0;k < sub.length();k++){
+                    if(sub.charAt(k) == '1'){
+                        break;
+                    }
+                    if(sub.charAt(k) == '0'){
+                        zeroCount++;
+                    }
+                }
             }else {
                 sub = builder.substring(i,i + 8);
             }
@@ -120,6 +144,119 @@ public class HuffmanCode {
         Map<Byte, String> huffmanCode = getCode(root);
         byte[] result = zip(former, huffmanCode);
         return result;
+    }
+    /**
+     * 将字节转化为二进制字符串
+     * @param b    需要转化的字节
+     * @param flag 是否为最后一个字节
+     * @return
+     */
+    public static String toBitString(byte b,boolean flag){
+        int temp = b;
+        if(!flag){
+            temp |= 256;
+        }
+        String string = Integer.toBinaryString(temp);
+        if(!flag){
+            return string.substring(string.length() - 8);
+        }else {
+            for (int i = 0;i < zeroCount;i++){
+                string = "0" + string;
+            }
+            return string;
+        }
+    }
+    /**
+     * 解码
+     * @param huffmanCodes
+     * @param bytes
+     * @return
+     */
+    public static byte[] decode(Map<Byte,String> huffmanCodes,byte[] bytes){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0;i < bytes.length;i++){
+            boolean flag = (i == bytes.length - 1);
+            stringBuilder.append(toBitString(bytes[i], flag));
+        }
+        HashMap<String, Byte> huffmanDecodes= new HashMap<>();
+        for (Map.Entry<Byte, String> entry : huffmanCodes.entrySet()) {
+            huffmanDecodes.put(entry.getValue(), entry.getKey());
+        }
+        ArrayList<Byte> list = new ArrayList<>();
+        for (int j = 0;j < stringBuilder.length();){
+            int count = 1;
+            boolean isValue = true;
+            Byte b = null;
+            while (isValue){
+                String substring = stringBuilder.substring(j, j + count);
+                b = huffmanDecodes.get(substring);
+                if(b != null){
+                    isValue = false;
+                }else {
+                    count++;
+                }
+            }
+            list.add(b);
+            j += count;
+        }
+        byte[] resByte = new byte[list.size()];
+        for (int k = 0;k < list.size();k++){
+            resByte[k] = list.get(k);
+        }
+        return resByte;
+    }
+    //压缩文件
+    public static void zipFile(String input,String output){
+        FileInputStream fileInputStream = null;
+        OutputStream outputStream = null;
+        ObjectOutputStream objectOutputStream = null;
+        try {
+            fileInputStream = new FileInputStream(input);
+            outputStream = new FileOutputStream(output);
+            objectOutputStream = new ObjectOutputStream(outputStream);
+            byte[] bytes = new byte[fileInputStream.available()];
+            fileInputStream.read(bytes);
+            byte[] zip = huffmanZip(bytes);
+            objectOutputStream.writeObject(zip);
+            objectOutputStream.writeObject(huffmanCodes);
+            objectOutputStream.writeObject(zeroCount);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                fileInputStream.close();
+                outputStream.close();
+                objectOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    //解压文件
+    public static void unZip(String input,String output){
+        FileInputStream fileInputStream = null;
+        ObjectInputStream objectInputStream = null;
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileInputStream = new FileInputStream(input);
+            objectInputStream = new ObjectInputStream(fileInputStream);
+            fileOutputStream = new FileOutputStream(output);
+            byte[] bytes = (byte[]) objectInputStream.readObject();
+            Map<Byte, String> map = (Map<Byte, String>) objectInputStream.readObject();
+            zeroCount = (int) objectInputStream.readObject();
+            byte[] decode = decode(map, bytes);
+            fileOutputStream.write(decode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                fileInputStream.close();
+                objectInputStream.close();
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
